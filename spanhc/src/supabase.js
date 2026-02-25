53eaa3233e0f99012b7e6e3b20920c702899ea4b
Ahmad Abdullahi Bayero <125829469+elakhmad2020@users.noreply.github.com>
2026-02-25 11:29:31 +0000

    Json correction

--- spanhc/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://ssmjjtbvrakzfsxezavp.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_GayEMildDmOJsQB6QW5pjw_GrhTDrQE';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
